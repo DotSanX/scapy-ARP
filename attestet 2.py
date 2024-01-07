c851ee5a036d4564901f8783af0088ba3d7d4ab1
DotSan <125751005+DotSanX@users.noreply.github.com>
2024-01-07 05:18:53 -0500

    Add files via upload

---
+++ attestet 2.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+scan_result = scan("10.10.10.0/24")
+
+scan_to_file = "".join(map(str, scan_result))
+f = open("scan_result.txt", "w")
+f.write(scan_to_file)
